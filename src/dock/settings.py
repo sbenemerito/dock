daa22607afeac2ae4098ff7f1a467bf99a8f96b1
samben <benemeritosam@gmail.com>
2017-03-18 15:28:56 +0800

    Ready for deployment

--- src/dock/settings.py
+++ src/dock/settings.py
@@ -127,3 +127,21 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+     'default': dj_database_url.config(default='postgres://localhost')
+     }
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "shortener/static"),
+    '/var/www/static/',
+    )
